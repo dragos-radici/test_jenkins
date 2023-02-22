6955be9b030dabdc878a28daf01e094cddee45cd
dragos-radici <dragos.radici@imgtec.com>
2023-02-22 16:54:29 +0200

    added last modded file

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # test_capitalize.py
 
 def capital_case(x):
-    if not isinstance(x, str):
-        raise TypeError('Please provide a string argument')
-      return x.capitalize()
+    return x
+
+def test_capital_case():
+    assert capital_case('semaphore') == 'Semaphore'
