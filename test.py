d8f48f18033494a1784419927a2c4f5a9c634d73
dragos-radici <dragos.radici@imgtec.com>
2023-02-22 16:43:11 +0200

    added last modded file

--- test.py
+++ test.py
@@ -1,1 +1,6 @@
-print('Hello, World!')
+# test_capitalize.py
+
+def capital_case(x):
+    if not isinstance(x, str):
+        raise TypeError('Please provide a string argument')
+      return x.capitalize()
